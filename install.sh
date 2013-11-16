8286bd79610941ac9f47df90d1cfdea6c8aa284e
Todd Kennedy <todd@selfassembled.org>
2013-11-16 13:28:35 -0500

    fixing installer

--- install.sh
+++ install.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
-SUBLIME_USER_DIR=/Library/Application\ Data/Sublime\ Text\ 3/Packages/User
+SUBLIME_USER_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
+PREF_PATH=$HOME/Library/Preferences/
+CURR_DIR=$(pwd)
 
-if [[ -d  "$SUBLIME_USER_DIR"]]; then
-  ln -s sublime/* $SUBLIME_USER_DIR
+if [[ -d  "$SUBLIME_USER_DIR" ]]; then
+  for SUBLIME_FILE in sublime/*; do
+    SUB_FILE=$(echo $SUBLIME_FILE | cut -d '/' -f2)
+    if [[ ! -e "$SUBLIME_USER_DIR$SUB_FILE" ]]; then
+      rm "$SUBLIME_USER_DIR$SUB_FILE"
+    fi
+    ln -s "$CURR_DIR/$SUBLIME_FILE" "$SUBLIME_USER_DIR$SUB_FILE"
+  done
 fi
 
 for DOTFILE in bash/*; do
-  ln -s $DOTFILE ~/.$DOTFILE
+  DOT=$(echo $DOTFILE | cut -d '/' -f2)
+  if [[ ! -e $HOME/.$DOT ]]; then
+    rm $HOME/.$DOT
+  fi
+  ln -s $CURR_DIR/$DOTFILE $HOME/.$DOT
 done
 
 for PREF in preferences/*; do
-  ln -s $PREF ~/Library/Preferences/$pref
+  PREFFILE=$(echo $PREF | cut -d '/' -f2)
+  if [[ ! -e "$PREF_PATH$PREFFILE" ]]; then
+    rm "$PREF_PATH$PREFFILE"
+  fi
+  ln -s $CURR_DIR/$PREF "$PREF_PATH$PREFFILE"
 done
-
