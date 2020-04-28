0bc2e44564ce00efe4716eff26158dad2d6f17df
Todd Kennedy <todd@selfassembled.org>
2020-04-28 15:59:47 -0700

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
-if [ -z "${LOGGING_DOTFILES_SETUP:-}" ] ; then
-  echo "# $(date)" > "$HOME/.dotfiles-setup.log"
-  LOGGING_DOTFILES_SETUP=1 exec "$0" >> "$HOME/.dotfiles-setup.log" 2>&1
-fi
-
-set -x
+DOTFILE_SRC=$(dirname "${BASH_SOURCE[0]}")
 
 mkdir -p $HOME/.vim
 if [ -L $HOME/.vim/coc-settings.json ]; then
   echo "${HOME}/.vim/coc-settings.json exists!"
 else
+  echo "Removing ${HOME}/.vim/coc-settings.json}"
   rm -f $HOME/.vim/coc-settings.json
+  echo "Linking $PWD/coc-settings.json to ${HOME}/.vim/coc-settings.json"
   ln -s $PWD/coc-settings.json $HOME/.vim/coc-settings.json
 fi
 
-for DOTFILE in $(find $PWD -type f -name ".*"); do
+for DOTFILE in $(find $DOTFILE_SRC -type f -name ".*"); do
   FN=$(basename $DOTFILE) 
   if [ -L $HOME/$FN ]; then
     echo "${HOME}/${FN} exists!"
   else
+    echo "Removing ${HOME}/${FN}"
     rm -f $HOME/$FN
+    echo "Linking ${DOTFILE} to ${HOME}/${FN}"
     ln -s $DOTFILE $HOME/$FN
   fi
 done
