f7c5f2ccbb127564384c897dc7e7dd4c585b86f0
Todd Kennedy <todd@selfassembled.org>
2021-11-03 11:29:18 -0700

    add host specific stuff

--- setup.sh
+++ setup.sh
@@ -8,13 +8,24 @@ OS_NAME=$(uname -s | tr A-Z a-z)
 ARCH=$(uname -m)
 if command -v lsb_release >> /dev/null; then
   DISTRO=$(lsb_release -is | tr A-Z a-z)
+else
+  echo "Missing lsb_release tool"
+  if [ "$OS_NAME" = "linux" ]; then
+    exit 1
+  fi
+fi
+
+if command -v hostnamectl >> /dev/null; then
+  HOSTNAME=$(hostnamectl hostname)
+else
+  HOSTNAME=$(hostname)
 fi
 
 NODE_VERSION="16.13.0"
 PYTHON_VERSION="3.10.0"
 ASDF_VERSION="0.8.1"
 FZF_VERSION=0.27.3
-NEOVIM_VERSION
+NEOVIM_VERSION=0.5.0
 
 echo "Script source: ${DOTFILE_SRC}, OS: ${OS_NAME}, ARCH: ${ARCH}, DISTRO: ${DISTRO}"
 
@@ -42,7 +53,7 @@ install_os_packages () {
       popd
       popd
       echo "-> Installing packages from AUR"
-      pacaur -S --noedit --noconfirm --needed aur-pkglist.txt
+      pacaur -S --noedit --noconfirm --needed $(cat aur-pkglist.txt)
     elif [ "$DISTRO" = "ubuntu" ] || [ "${DISTRO}" = "debian" ]; then
       # needed for appimages to work
       sudo apt install -y base-devel git libfuse2 libssl-dev xz-utils
@@ -50,7 +61,7 @@ install_os_packages () {
     echo "-> Installing neovim appimage"
     # Install neovim
     mkdir -p $HOME/bin
-    curl -L -o $HOME/bin/nvim https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage
+    curl -L -o $HOME/bin/nvim https://github.com/neovim/neovim/releases/download/$NEOVIM_VERSION/nvim.appimage
     chmod a+x $HOME/bin/nvim
   else
     "-> Installing xcode command line tools"
@@ -77,6 +88,7 @@ setup_rust() {
   else
     echo "installing rust toolchain"
     sh ./install-rust.sh --default-toolchain stable -y
+    source $HOME/.cargo/env
   fi
 
   if [ -f $DOTFILE_SRC/$OS_NAME-$ARCH-bin.tar.xz ]; then
@@ -90,48 +102,60 @@ setup_rust() {
       cargo install $PKG
     done
   fi
-
-	  if [ -L $HOME/.config/starship.toml ]; then
-	    echo "${HOME}/.config/starship.toml exists!"
-	  else
-	    echo "Removing ${HOME}/.config/starship.toml"
-	    rm -f $HOME/.config/starship.toml
-	    echo "Linking ${DOTFILE_SRC}/.config/starship.toml to ${HOME}/.config/starship.toml"
-	    ln -s $DOTFILE_SRC/.config/starship.toml $HOME/.config/starship.toml
-	  fi
   echo "CONFIGURED RUST"
 }
 
 setup_dotdirs () {
-  DOT_DIRS=$(find $DOTFILE_SRC -type f -name ".*" | grep -v "git")
+  DOT_DIRS=$(find $DOTFILE_SRC -type d -name ".*" | grep -v "git")
+  echo "SETTING UP DOTDIRS"
   for DOT_DIR in $DOT_DIRS; do
-    for CONFIG_FILE in $(find $DOTFILE_SRC/$DOT_DIR -type f); do
-      DEST_FILE=$HOME/$(echo $CONFIG_FILE | sed "s|${DOTFILE_SRC}||")
+    for CONFIG_FILE in $(find $DOT_DIR -type f); do
+      DEST_FILE=$HOME/$(echo $CONFIG_FILE | sed "s|${DOTFILE_SRC}/||")
       DEST_DIR=$(dirname $DEST_FILE)
+      SRC_DIR=$(dirname $CONFIG_FILE)
       mkdir -p $DEST_DIR
-      if [ -L $DEST_FILE ]; then
-        echo "${DEST_FILE} already exists!"
+
+      if [ "$CONFIG_FILE" = "$SRC_DIR/$HOSTNAME" ]; then
+        HOST_SPECIFIC_CONFIG=$DEST_DIR/current-hostname
+        if [ -L $HOST_SPECIFIC_CONFIG ]; then
+          echo "${HOST_SPECIFIC_CONFIG} already exists!"
+        else
+          echo "Removing ${HOST_SPECIFIC_CONFIG}"
+          rm -f $HOST_SPECIFIC_CONFIG
+          echo "Linking ${CONFIG_FILE} to ${HOST_SPECIFIC_CONFIG}"
+          ln -s $CONFIG_FILE $HOST_SPECIFIC_CONFIG
+        fi
       else
-        echo "Removing ${DEST_FILE}"
-        rm -rf $DEST_FILE
-        echo "Linking ${CONFIG_FILE} to ${DEST_FILE}"
-        ln -s $CONFIG_FILE $DEST_FILE
+        if [ -L $DEST_FILE ]; then
+          echo "${DEST_FILE} already exists!"
+        else
+          echo "Removing ${DEST_FILE}"
+          rm -f $DEST_FILE
+          echo "Linking ${CONFIG_FILE} to ${DEST_FILE}"
+          ln -s $CONFIG_FILE $DEST_FILE
+        fi
       fi
     done
   done
+  echo "DONE SETTING UP DOTDIRS"
 }
 
 setup_asdf () {
-  echo "CONFIGURING ASDF"
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch "v${ASDF_VERSION}"
-  . $HOME/.asdf/asdf.sh
-  asdf plugin add python
-  asdf install python $PYTHON_VERSION
-  asdf global python $PYTHON_VERSION
-  asdf plugin add nodejs
-  asdf install nodejs $NODE_VERSION
-  asdf global nodejs $NODE_VERSION
-  echo "DONE CONFIGURING ASDF"
+  if [ ! -d $HOME/.asdf ]; then
+    echo "CONFIGURING ASDF"
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch "v${ASDF_VERSION}"
+    . $HOME/.asdf/asdf.sh
+    asdf plugin add python
+    asdf install python $PYTHON_VERSION
+    asdf global python $PYTHON_VERSION
+    asdf plugin add nodejs
+    asdf install nodejs $NODE_VERSION
+    asdf global nodejs $NODE_VERSION
+    echo "DONE CONFIGURING ASDF"
+  else
+    echo "ASDF ALREADY CONFIGURED"
+    . $HOME/.asdf/asdf.sh
+  fi
 }
 
 setup_dotfiles () {
@@ -173,7 +197,12 @@ setup_ssh () {
 }
 
 # we need python installed first...
-install_os_packages
+if [ ! -f $HOME/.no-os-packages ]; then
+  install_os_packages
+else
+  echo "NOT INSTALLING OS PACKAGES"
+fi
+
 setup_asdf
 setup_dotfiles
 setup_dotdirs
